1201c5de9e85c689d1dedfdf2a4797f15768d7ef
Claudia Matosa <claudiamatosa@users.noreply.github.com>
2020-03-04 17:51:52 +0000

    add debugging statements

--- index.js
+++ index.js
@@ -15,16 +15,25 @@ async function run() {
 
     const body = github.context.payload.pull_request.body;
 
+    core.debug('initial description: ', body);
+
     if (!body) return;
 
     const newBody = (body.match(/{{\w+}}/g) || '').reduce((currentBody, variable) => {
+      core.debug('variable: ', variable);
+
       const variableName = variable.replace(/({|})/g, '');
       const replacement = variables(variableName);
+
+      core.debug('replacement: ', replacement);
+      core.debug('current description: ', currentBody);
 
       if(!replacement) return body;
 
       return currentBody.replace(variable, replacement);
     }, body);
+
+    core.debug('new description: ', newBody);
 
     const request = {
       owner: github.context.repo.owner,
