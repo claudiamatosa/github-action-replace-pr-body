ed45aefbb100284df7fc84dc3158cd4c4bde5d98
Claudia Matosa <claudiamatosa@users.noreply.github.com>
2020-03-05 15:04:40 +0000

    Update PR

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+// Toolkit docs: https://github.com/actions/toolkit
+
 async function run() {
   try {
     const inputs = {
-      token: core.getInput('github-token', {required: true}),
-      variables: core.getInput('github-token', {required: true})
+      token: core.getInput('github-token', {required: true})
     };
 
     // Pull-request format: https://developer.github.com/v3/pulls/#response
@@ -19,7 +20,7 @@ async function run() {
 
     if (!body) return;
 
-    const newBody = (body.match(/{{\w+}}/g) || '').reduce((currentBody, variable) => {
+    const newBody = (body.match(/{{\w+}}/g) || '').reduce((contents, variable) => {
       console.log(typeof variable);
       console.log('variable: ', variable);
 
@@ -38,7 +39,7 @@ async function run() {
 
       // return currentBody.replace(variable, replacement);
 
-      return currentBody.replace('{{prNumber}}', variables.prNumber);
+      return contents.replace('{{prNumber}}', variables.prNumber);
     }, body);
 
     console.log('new description: ', newBody);
@@ -47,13 +48,11 @@ async function run() {
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       pull_number: github.context.payload.pull_request.number,
-      body
+      body: newBody
     };
 
     const client = new github.GitHub(inputs.token);
     const response = await client.pulls.update(request);
-
-    console.log(response.status);
 
     if (response.status !== 200) {
       core.error('There was an issue while trying to update the pull-request.');
